84bf49a6219945b601e84ade0056392fd3dae60a
rockysingh6769 <rockysingh6769@gmail.com>
2019-01-29 17:06:44 +0530

    change in live clock file

--- live_clock.php
+++ live_clock.php
@@ -4,27 +4,22 @@
 </head>
 <body onload=display_ct();>
 
-
 <form name="Tick">
-<input type="text" size="12" name="Clock">
-<span id='ct' ></span>
+  <input type="text" size="12" name="Clock">
 </form>
 
 </body>
 <script type="text/javascript"> 
-function display_c(){
-var refresh=1000; // Refresh rate in milli seconds
-mytime=setTimeout('display_ct()',refresh)
+function display_c()
+{ var refresh=1000; 
+  mytime=setTimeout('display_ct()',refresh)
 }
-
 function display_ct() 
 {
   var x = new Date()
   var hours=x.getHours();
   var minutes=x.getMinutes();
   var seconds=x.getSeconds();
-  //var x1=x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getYear(); 
-  //var x1 = x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds();
   if(hours>11)
   {   dn="PM"
       hours=hours-12
@@ -37,8 +32,9 @@ function display_ct()
       seconds=seconds
   document.Tick.Clock.value=
   hours+":"+minutes+":"+seconds+" "+dn;
-  document.getElementById('ct').innerHTML = hours+":"+minutes+":"+seconds+" "+dn;
   display_c();
 }
 </script>
 </html>
+
+
